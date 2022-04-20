85414668f91b9ce73268e1560d9b22a52c09e0fa
Nithya-24 <nithyaramadoz@gmail.com>
2022-04-20 22:31:08 +0530

    Added: Populating AddressBookList from Localstorage

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,49 +1,42 @@
-
+let contactList;
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    document.querySelector(".contact-count").textContent = contactList.length;
-    createInnerHtml();
+  contactList = getContactFromLocalStorage();
+  document.querySelector(".contact-count").textContent = contactList.length;
+  createInnerHtml();
 });
 
 const createInnerHtml = () => {
-    if (contactList.length == 0) {
-        return;
-    }
-    const headerHtml = `<tr>
-      <th>Name</th>
-      <th>Address</th>
-      <th>City</th>
-      <th>State</th>
-      <th>Zip Code</th>
-      <th>Phone Number</th>
-      </tr>`;
-    let innerHtml = `${headerHtml}`;
-    for (let contact of contactList) {
-        innerHtml = `${innerHtml} 
-          <tr>
-          <td>${contact._name}</td>
-          <td>${contact._address}</td>
-          <td>${contact._city}</td>
-          <td>${contact._state}</td>
-          <td>${contact._zip}</td>
-          <td>${contact._phoneNumber}</td>
-          <td>
-              <img src="../assets/delete-black-18dp.svg" alt="delete" id="${contact._id}" onclick="remove(this)"/>
-              <img src="../assets/create-black-18dp.svg" alt="update" id="${contact._id}" onclick="update(this)"/>
-          </td>
-          </tr>`;
-    }
-    document.querySelector("#table-display").innerHTML = innerHtml;
+  if (contactList.length == 0) {
+    return;
+  }
+  const headerHtml = `<tr>
+    <th>Full Name</th>
+    <th>Address</th>
+    <th>City</th>
+    <th>State</th>
+    <th>Zip Code</th>
+    <th>Phone Number</th>
+    </tr>`;
+  let innerHtml = `${headerHtml}`;
+  for (let contact of contactList) {
+    innerHtml = `${innerHtml} 
+        <tr>
+        <td>${contact._name}</td>
+        <td>${contact._address}</td>
+        <td>${contact._city}</td>
+        <td>${contact._state}</td>
+        <td>${contact._zip}</td>
+        <td>${contact._phoneNumber}</td>
+        <td>
+            <img src="../assets/delete-black-18dp.svg" alt="delete" id="${contact._id}" onclick="remove(this)">
+            <img src="../assets/create-black-18dp.svg" alt="update" id="${contact._id}" onclick="update(this)">
+        </td>
+        </tr>`;
+  }
+  document.querySelector("#table-display").innerHTML = innerHtml;
 };
 
-let contactList = [{
-        _id: 1,
-        _name: "Nithya R",
-        _phoneNumber: "91 7894561320",
-        _address: "satchiyapuram road",
-        _city: "Bangalore",
-        _state: "Karnataka",
-        _zip: "123456",
-    },
-    
-];
+const getContactFromLocalStorage = () =>{
+  return localStorage.getItem('ContactList') ? JSON.parse(localStorage.getItem('ContactList')) : []
+}
